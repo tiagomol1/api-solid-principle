23a9ed12b8c2c0f1cd8e7ae40e7f80fba60236f3
Tiago Murilo Ochoa da Luz <tiago.murilo08@gmail.com>
2024-01-23 13:41:34 -0300

    first commit, with unit test

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,38 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password
+    })
+
+    return reply.status(201).send()
+
+  } catch (error) {
+    if(error instanceof UserAlreadyExistsError){
+      return reply.status(409).send({message: error.message})
+    }
+
+    throw error
+  }
+
+}
---
+++ src/http/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from 'fastify'
+import { register } from './controllers/register'
+
+export async function appRoutes(app: FastifyInstance){
+
+  app.post('/users', register)
+
+}
+
